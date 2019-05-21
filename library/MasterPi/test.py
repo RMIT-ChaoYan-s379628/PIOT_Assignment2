22bf79539fd42285548cd521e0e91c2ddbc2a8af
RMIT-ChaoYan-s379628 <s3579628@student.rmit.edu.au>
2019-05-21 13:18:50 +1000

    remove files

--- library/MasterPi/test.py
+++
@@ -1,9 +0,0 @@
-import pymysql
-
-connection = pymysql.connect(host='35.244.109.255',
-                                 user='root',
-                                 password='65390057y',
-                                 db='LMS')
-with connection.cursor() as cursor:
-    cursor.execute("INSERT INTO `LMS`.`LmsUser` (`LmsUserID`, `UserName`, `Name`) VALUES ('1', 'test2', 'test2');")
-    connection.commit()
