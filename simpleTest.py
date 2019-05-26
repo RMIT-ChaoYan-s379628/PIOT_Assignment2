c158c6b9b6e8fd57837bae206324dafc5adc965a
Mav!s <s3621134@student.rmit.edu.au>
2019-05-26 18:44:46 +1000

    modify

--- simpleTest.py
+++ simpleTest.py
@@ -1,27 +1,26 @@
-import unittest  # 导入unittest  包
-from unittest_doc.com.Calculator.Calculator import calculator  # 引入需要测试的包
+import unittest
+from unittest_doc.com.Calculator.Calculator import calculator
 
-# 所有用例需继承TestCase类或者其子类
 class simple_test(unittest.TestCase):
    def setUp(self):
-       print('--- 初始化test_simple ---')
+       print('--- initialise test_simple ---')
        self.a = calculator(1, 2)
 
    def test_add(self):
-       print('--- 测试用例test_simple add ---')
-       self.assertEqual(self.a.minus(), -1, '两值不相等')
-       self.assertEqual(self.a.add(), 3, '两值不相等')
-       self.assertNotEqual(self.a.divide(), 1, '两值不相等')
+       print('--- sample test_simple add ---')
+       self.assertEqual(self.a.minus(), -1, 'equal')
+       self.assertEqual(self.a.add(), 3, 'not equal')
+       self.assertNotEqual(self.a.divide(), 1, 'not equal')
 
    def test_divide(self):
-       print('--- 测试用例test_simple divide ---')
+       print('--- samle test_simple divide ---')
        self.assertEqual(self.a.divide(), 0.5)
 
    def tearDown(self):
-       print('--- 结束test_simple ---')
+       print('--- finish test_simple ---')
 
-def suite():  # 创建测试添加测试套件函数
-   suite = unittest.TestSuite()  # 建立测试套件
+def suite():  # Create a test add test suite function
+   suite = unittest.TestSuite()  # establish test conditions
    suite.addTests([simple_test('test_add'), simple_test('test_divide')])
 return suite
 
